5ada6fa084092df48120bf4b5830018b75c0b66f
admin <basirgazhin@edu.hse.ru>
2025-12-14 11:30:01 +0300

    feat(config): Update training parameters

--- defects_in_welds/data/data_module.py
+++ defects_in_welds/data/data_module.py
@@ -255,7 +255,7 @@ class DefectDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=True,
             collate_fn=self.collate_fn,
-            num_workers=0,
+            num_workers=2,
         )
 
     def val_dataloader(self) -> DataLoader:
@@ -264,7 +264,7 @@ class DefectDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=False,
             collate_fn=self.collate_fn,
-            num_workers=0,
+            num_workers=2,
         )
 
     def test_dataloader(self) -> DataLoader:
@@ -273,7 +273,7 @@ class DefectDataModule(pl.LightningDataModule):
             batch_size=self.batch_size,
             shuffle=False,
             collate_fn=self.collate_fn,
-            num_workers=0,
+            num_workers=2,
         )
 
 
